308c16a48fbcb7ac2555f114c97f74405f70a839
Ivan V <ivanv@Ivans-MacBook-Pro.local>
2019-10-19 14:50:47 +1100

    Added specific styles to Event.vue

--- frontend/src/views/Event.vue
+++ frontend/src/views/Event.vue
@@ -4,7 +4,7 @@
     <h2>Your event code is {{ this.eid }}</h2>
     <EventQRCode v-bind:eid="this.eid" />
     <div class="form-container">
-      <form class="form" id="create-event-form" action>
+      <form action>
         <h3>{{ eventURL }}</h3>
         <div class="label-input-div">
           <label for>Enter zID</label>
