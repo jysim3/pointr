f3d3afcdab850de313457242bd339349d7801f12
Ivan V <ivanv@Ivans-MacBook-Pro.local>
2019-10-19 13:06:15 +1100

    EventQRCode component done

--- frontend/src/components/EventQRCode.vue
+++ frontend/src/components/EventQRCode.vue
@@ -1,7 +1,6 @@
 <template>
   <div>
     <canvas id="qr-canvas"></canvas>
-    <h1>{{ this.eid }}</h1>
   </div>
 </template>
 
@@ -12,26 +11,16 @@ export default {
   name: "EventQRCode",
   props: ["eid"],
   mounted() {
-    // const generateQR = async text => {
-    //   await QRCode.toDataURL(text)
-    //   // try {
-    //   //   console.log();
-    //   // } catch (err) {
-    //   //   console.error(err);
-    //   // }
-    // };
-
-    // const addQRCode = async text => {
-    //   const canvas = document.getElementById("qr-canvas")
-    //   const qrcode = generateQR(text)
-    //   canvas.appendChild(qrcode)
-    // }
-
-    // addQRCode(this.eid)
+    // Generating and adding QRCode to canvas
     const canvas = document.getElementById("qr-canvas")
     const url = `${window.location.host}/e/${this.eid}`
     QRCode.toCanvas(canvas, url);
-    console.log(url) //eslint-disable-line
+    // canvas.style.height = '10rem'
+    // canvas.style.width = '10rem'
+    // console.log(url) //eslint-disable-line
   }
 };
 </script>
+
+<style>
+</style>
