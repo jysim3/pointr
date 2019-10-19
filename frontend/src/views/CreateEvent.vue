d0ccf82f12f7a32cc4f4e3dfd50b73610ae83991
Ivan V <ivanv@Ivans-MacBook-Pro.local>
2019-10-19 14:02:57 +1100

    Added fonts and styles

--- frontend/src/views/CreateEvent.vue
+++ frontend/src/views/CreateEvent.vue
@@ -1,45 +1,43 @@
 <template>
-    <div>
-        <h1>pointer</h1>
+  <div>
+    <h1>pointer</h1>
+    <div class="form-container">
+      <form @submit="submitForm">
         <h2>Create Event</h2>
-        <form @submit="submitForm">
-            <div class="label-input-div">
-                <label for="">Event title</label>
-                <input v-bind="title" type="text">
-            </div>
-            <div class="label-input-div">
-                <label for="">Society</label>
-                <input v-bind="society" type="text">
-            </div>
-            <div class="label-input-div">
-                <label for="">Set default points</label>
-                <input v-bind="point" type="text">
-            </div>
-            <div class="label-input-div">
-                <label for="">Show QR Code/Event link</label>
-                <input type="checkbox">
-            </div>
+        <div class="label-input-div">
+          <label for>Event title</label>
+          <input v-bind="title" type="text" />
+        </div>
+        <div class="label-input-div">
+          <label for>Society</label>
+          <input v-bind="society" type="text" />
+        </div>
+        <div class="label-input-div">
+          <label for>Set default points</label>
+          <input v-bind="point" type="text" />
+        </div>
+        <div class="label-input-div">
+          <label for>Show QR Code/Event link</label>
+          <input type="checkbox" />
+        </div>
 
-            <button @click="submitForm" class="btn-primary">Create Event</button>
-        </form>
+        <button @click="submitForm" class="btn-primary">Create Event</button>
+      </form>
     </div>
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'CreateEvent',
-    methods: {
-        submit(e) {
-            e.preventDefault();
-
-
-        }
+  name: "CreateEvent",
+  methods: {
+    submit(e) {
+      e.preventDefault();
     }
-}
+  }
+};
 </script>
 
 <style>
-    .label-input-div label {
-        display: block;
-    }
+@import "../assets/style.css";
 </style>
