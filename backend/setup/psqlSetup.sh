32bd551c47e3e33bce3a268323c80dc07611e757
Steven Shen <stevenshen1999@hotmail.com>
2020-02-07 22:25:54 +1100

    Fixed a bug with the setup script for psql

--- backend/setup/psqlSetup.sh
+++ backend/setup/psqlSetup.sh
@@ -2,3 +2,6 @@
 sudo apt-get install postgresql
 sudo apt-get install python-psycopg2
 sudo apt-get install libpq-dev
+sudo -u postgres createuser --superuser $USER
+pip3 install psycopg2
+pip3 install -r ../requirements.txt
