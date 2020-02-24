4093277ad51c1dcb057a8499e7e85c98cb842138
martejj <steynharrison1@gmail.com>
2020-02-24 20:41:54 +1100

    From now on to run server set environment variables POINTR_EMAIL_PASSWORD and POINTR_SERVER_SECRET (For jwt tokens) or use the ./run.sh script

--- backend/run.sh
+++ backend/run.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
+if test $# != 2
+then
+    echo "Execute as $0 '[POINTR_EMAIL_PASSWORD]' '[POINTR_SERVER_SECRET]'"
+    exit 1
+fi
+
+export POINTR_EMAIL_PASSWORD="$1"
+export POINTR_SERVER_SECRET="$2"
 export FLASK_APP=app.py 
 export FLASK_ENV=development 
 flask run
