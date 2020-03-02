7e5e77ea02d0b27f15825e0020c8e5f520a646be
Ivan-Velickovic <10481259+Ivan-Velickovic@users.noreply.github.com>
2020-03-03 03:09:01 +1100

    Fixed and added 'please activate from email' message in the case user sign in without activating first

--- frontend/src/views/auth/AccountActivation.vue
+++ frontend/src/views/auth/AccountActivation.vue
@@ -1,25 +1,25 @@
 <template>
   <div class="card-container">
     <div class="card">
-      <h1>Hi {{ this.name }} ({{ this.zID }})!</h1>
-      <p class="msg" v-if="isActivated.status === 200">
+      <h1>Hi {{ this.$store.state.user.name }} ({{ this.$store.state.user.zID }})!</h1>
+      <p v-if="!activateToken">Please go to your email to activate your account first.</p>
+      <p class="msg" v-if="isActivatedStatus === 200">
         Thanks for activating your account, you may now close this window or
         <router-link to="/signin">sign in</router-link>.
       </p>
-      <p class="msg" v-else-if="isActivated.status === 403">
+      <p class="msg" v-else-if="isActivatedStatus === 403">
         Your account has already been activated! You can sign in
         <router-link to="/signin">here</router-link>
       </p>
       <p
         class="msg"
         v-else
-      >An error was encountered when trying to activate your account. If the issue persists, please <router-link to="/contact">contact us</router-link>. {{ isActivated.msg }}</p>
+      >An error was encountered when trying to activate your account. If the issue persists, please <router-link to="/contact">contact us</router-link>.</p>
     </div>
   </div>
 </template>
 
 <script>
-import jwt from "jsonwebtoken";
 import { fetchAPI } from "@/util";
 
 export default {
@@ -27,29 +27,26 @@ export default {
   props: {
     activateToken: {
       type: String,
-      required: true
+      required: false
     }
   },
   data() {
     return {
       zID: "",
       name: "",
-      isActivated: {
-        status: null,
-        msg: ""
-      }
+      isActivatedStatus: "",
     };
   },
-  created() {
-    const decodedToken = jwt.decode(this.activateToken);
-    // this.name = decodedToken["name"]; TODO: need to do an info request
-    this.zID = decodedToken["zID"];
-
-    fetchAPI(`/api/auth/activate?token=${this.activateToken}`).then(r => {
-      this.isActivated.status = r.status;
-      this.isActivated.msg = r.data.message;
-      console.log("r is" + r); //eslint-disable-line
-    });
+  async created() {
+    if (this.activateToken) {
+      try {
+        const response = await fetchAPI(`/api/auth/activate?token=${this.activateToken}`, "POST")
+        this.isActivatedStatus = response.status;
+        this.isActivated.msg = response.data.message;
+      } catch (error) {
+        this.isActivatedStatus = error.response.status;
+      }
+    }
   }
 };
 </script>
