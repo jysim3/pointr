b2249b33fc4f33f141a6c3a136e1aea343577293
Ivan-Velickovic <10481259+Ivan-Velickovic@users.noreply.github.com>
2020-03-03 12:43:12 +1100

    Changed styling of event card

--- frontend/src/components/EventCard.vue
+++ frontend/src/components/EventCard.vue
@@ -1,11 +1,18 @@
 <template>
   <div class="event-card">
     <!-- TODO: going to an event should only be available to admin? -->
-    <router-link :to="`/event/${eventData.eventID}`">
+    <!-- <router-link :to="`/event/${eventData.eventID}`">
       <h3>{{ eventData.eventName }}</h3>
       <p class="event-info">by {{ eventData.societyName }} @ <b>{{ eventData.location }}</b></p>
       <p class="event-info">On {{ eventData.eventDate }}</p>
-    </router-link>
+    </router-link> -->
+    <!-- TODO: check backend, eventName/name doesn't seem to be consistent depending on request -->
+    <div class="when-where-div">
+      <p class="when">{{ eventData.eventDate }}</p>
+      <p class="where">@ {{ eventData.location }}</p>
+    </div>
+    <h3 class="name">{{ eventData.name }}</h3>
+    <p class="society">{{ eventData.societyName }}</p>
   </div>
 </template>
 
@@ -22,7 +29,8 @@ export default {
 </script>
 
 <style scoped>
-a {
+/* TODO: clean up this CSS */
+/* a {
   font-weight: normal;
 }
 .event-card {
@@ -32,5 +40,37 @@ a {
 
 .event-info {
   margin: 1rem 0;
+} */
+.event-card {
+  border: 3px solid var(--c-secondary-dark);
+  margin: 1rem;
+  padding: 1rem 1rem 1.25rem 1rem;
+  border-radius: var(--border-radius);
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+}
+
+.when-where-div {
+  margin-bottom: 1rem;
+  display: flex;
+  flex-wrap: wrap;
+}
+
+.when {
+  color: var(--c-primary);
+}
+
+.where {
+  padding-left: 0.5rem;
+}
+
+.name {
+  color: black;
+  margin-bottom: 0.25rem;
+}
+
+.society {
+  font-size: 1.1rem;
 }
 </style>
