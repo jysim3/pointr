a19df8c92a03c33ae37fb6da896c56506ad22bc3
Ubuntu <ubuntu@ip-172-31-18-92.us-east-2.compute.internal>
2020-03-11 23:52:27 +1100

    Final fix for time

--- backend/updateAttendanceTime.py
+++ backend/updateAttendanceTime.py
@@ -1,19 +1,15 @@
-import datetime
 #import pytz
 #from util.utilFunctions import makeConnection
 from util.utilFunctions import createConnection
-from dateutil import tz
-
-fromZone = tz.gettz('UTC')
-toZone = tz.gettz('Australia/Sydney')
+#from dateutil import tz
+from datetime import timedelta, datetime
 
 conn = createConnection()
 curs = conn.cursor()
 curs.execute("SELECT time, zid FROM participation;")
 results = curs.fetchall()
 for i in results:
-    time = i[0].replace(tzinfo=fromZone)
-    convertion = time.astimezone(toZone)
-    curs.execute("UPDATE PARTICIPATION SET TIME = (%s) WHERE ZID = (%s);", (convertion, i[1],))
+    time = i[0] + timedelta(hours=11)
+    curs.execute("UPDATE PARTICIPATION SET TIME = (%s) WHERE ZID = (%s);", (time, i[1],))
 
 conn.commit()
