61aaecc68a0a451bd50cded31189d32d64d8d1d8
Steven Shen <stevenshen1999@hotmail.com>
2020-03-22 00:59:28 +1100

    Updated the automateBackup.py script

--- backend/automateBackup.py
+++ backend/automateBackup.py
@@ -2,4 +2,4 @@ import os
 from datetime import datetime
 
 currTime = datetime.now().strftime("%d%m%Y_%H%M")
-os.system(f"pg_dumpall > ~/databaseBackups/{currTime}.bak")
+os.system(f"pg_dump pointrDB > ~/databaseBackups/{currTime}.bak")
