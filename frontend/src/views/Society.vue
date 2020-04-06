3e847c607d277aa9f290b9bdd0d9488256b77101
jysim3 <junyang.0607@gmail.com>
2020-04-06 19:00:27 +1000

    society page looks fucking good

--- frontend/src/views/Society.vue
+++ frontend/src/views/Society.vue
@@ -1,28 +1,62 @@
 <template>
     <div class="wrapper">
         <SelectSociety v-if="!socID"/>
-        <div v-else>
-
+        <div id="society-wrapper" v-else>
             <div class="header">
-              <!-- <h1 class="header-text" v-once>{{ socName }}</h1> -->
-              <img v-if="socData" :src="apiURL + socData.logo" />
+              <div class="profile" >
+                <div class="profile-info">
+
+                  <h2 class="header-text" v-once>{{ socName }}</h2>
+                <i class="material-icons profile-buttons-followers">favorite</i>
+<!-- TODO: MAKE THIS 'JOIN SOCIETY' -->
+                  <p> Society for the college UNSW Hall. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+
+                </div>
+                <img v-if="socData" :src="apiURL + socData.logo" />
+
+              </div>
+              <div class="profile-buttons">
+
+                <i class="material-icons profile-buttons-followers">person</i>
+                <span class="profile-buttons-followers">224 members</span>
+                <i class="material-icons profile-buttons-followers" style="color: purple">trending_up</i>
+                <span class="profile-buttons-followers">150 weekly active users</span>
+              </div>
             </div>
 
-            <EventList
-              :eventViewTitle="'Upcoming Events for ' + socName"
-              :eventData="societyEvents"
-              listStyle="table"
-            />
-            <EventList
-              :eventViewTitle="'Past Events for ' + socName"
-              :eventData="pastSocietyEvents"
-              listStyle="table"
-              :loading="pastEventsLoading"
-            />
-            <MakeAdmin v-if="isStaff" :socID="socID"/>
-            <!--- TODO: more features for admins-->
+            <div class="tabs-wrapper" >
+              <ul class="tabs">
+                <li class="tabs-item tabs-item--active">Home</li>
+                <li class="tabs-item">TBC</li>
+                <li class="tabs-item">TBC</li>
+                <li class="tabs-item">TBC</li>
+              </ul>
 
 
+            </div>
+            
+
+
+
+
+
+            <div class="main">
+              <div class="wrapper">
+                <EventList
+                  :eventViewTitle="'Upcoming Events for ' + socName"
+                  :eventData="societyEvents"
+                  listStyle="table"
+                />
+                <EventList
+                  :eventViewTitle="'Past Events for ' + socName"
+                  :eventData="pastSocietyEvents"
+                  listStyle="table"
+                  :loading="pastEventsLoading"
+                />
+                <MakeAdmin v-if="isStaff" :socID="socID"/>
+                <!--- TODO: more features for admins-->
+              </div>
+            </div>
         </div>
     </div>
 </template>
@@ -31,18 +65,82 @@
 .wrapper {
     margin: auto;
     width: 80%;
+    max-width: 900px;
 }
 .header {
-  text-align: center;
+  margin-top: 4rem;
 }
-.header h1 {
-  font-size: 4rem;
+.profile {
+  display: flex;
+  /* text-align: center; */
+  justify-content: space-between;
 }
-.header > img {
-  max-width: 100%;
-  border-radius: 10px ;
+.profile-info > h2 {
+  font-size: 2rem;
+  color: black;
+}
+.profile-info > i {
+  margin-left: 1rem;
+  color: red;
+}
+.profile-info > i,h2 {
+  display: inline;
+  vertical-align: middle;
+}
+.profile-info > p {
+  padding-top: 1rem;
+}
+.profile > img {
+  width: 150px;
+  object-fit: cover;
+  height: 150px;
   box-shadow:   0 0rem 2rem 0rem rgba(59,59,95,0.3);
-  border-radius: 10px ;
+  border-radius: 150px ;
+  margin: 0 3rem;
+}
+span.profile-buttons-followers {
+  margin-right: 1rem;
+}
+.profile-buttons-followers {
+  vertical-align: middle;
+}
+.tabs-wrapper, .tabs {
+  width: 100%;
+}
+.tabs-wrapper {
+  border-bottom: 1px solid rgba(0, 0, 0, 0.1);
+}
+.tabs {
+  list-style-type: none;
+  margin-bottom: -1px;
+  display: flex;
+  margin-top: 3rem;
+  padding: 0;
+}
+.tabs-item {
+  display: block;
+  flex: 1 1 0;
+  text-align: center;
+  margin-right: 2px;
+  padding: 0.5rem 1rem;
+  border-radius: 5px 5px 0 0 ;
+  background-color: #e3f2fd;
+  color: black;
+  max-width: 8rem;
+}
+.tabs-item--active {
+  background-color: white;
+}
+.tabs-item:hover {
+  background-color: var(--c-primary);
+}
+
+.main {
+  width: 100%;
+  left: 0;
+  position: absolute;
+  background: white;
+  padding: 0 3rem;
 }
 
 </style>
