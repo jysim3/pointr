86b12666e99cf0e6f39fd228aa9c178714561aad
jysim3 <junyang.0607@gmail.com>
2020-05-27 23:14:27 +1000

    event attendee in table

--- frontend/src/components/Table.vue
+++ frontend/src/components/Table.vue
@@ -14,11 +14,22 @@
                 :key="i" >
                     {{ event[n] }}
                 </td>
+                
           </tr>
         </table> 
     
 </template>
 <script>
+/*
+requires 
+field = ['names', 'of', 'the', 'data', 'key']
+data = {
+  'names': 'hello',
+  'of' : 'hihi',
+  ...
+
+  }
+*/
 export default {
     name: "Table",
     props: {
@@ -29,6 +40,10 @@ export default {
         data: {
             required: true,
             type: Array
+        },
+        actions: {
+          required: false,
+          type: Array
         }
     },
     methods: {
@@ -39,48 +54,5 @@ export default {
 }
 </script>
 <style scoped>
-table 			      { 
-  border-spacing: 1; 
-  border-collapse: collapse; 
-  background: transparent;
-  border-radius:6px;
-  overflow:hidden;
-  max-width:800px; 
-  width:100%;
-  margin:0 auto;
-  position:relative;
-  
-}
-table td,th           { 
-  padding-left:8px
-}
-
-table th        { 
-  height:60px;
-  background: var(--c-primary);
-  font-size:16px;
-  color: #e3f2fd;
-}
-
-table tr        { 
-    height:48px; 
-    border-bottom: 2px solid var(--c-secondary);
-    transition: 0.2s;
-    background: white;
-}
-table tr.link:hover{ 
-    background: #e3f2fd;
-    cursor: pointer;
-    transform: translate(0, -2px);
-    transition: 0.2s;
-}
-table tr:last-child  { 
-  border:0; 
-}
-
-table td,th { 
-  text-align:left;
-}
-
 
 </style>
