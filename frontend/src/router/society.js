52ebcd828f8e8a1f2e6d0c82ae40f11809d202b1
jysim3 <junyang.0607@gmail.com>
2020-06-13 15:18:22 +1000

    router changes

--- frontend/src/router/society.js
+++ frontend/src/router/society.js
@@ -1,10 +1,22 @@
 // import SocietyJoin from '@/views/SocietyJoin.vue';
-import Society from '@/views/Society.vue';
+import Society from '@/views/SocietyPage.vue';
+import SelectSociety from '@/views/SelectSociety.vue';
 export default [
   {
-    path: '/society/:socID?',
+    path: '/society/:socID',
     name: 'society',
     component: Society,
+    // TODO: make this available for public?
+    props: true,
+    meta: {
+      requiresAuth: true,
+      title: 'Society Page - Pointr'
+    }
+  },
+  {
+    path: '/society',
+    name: 'society',
+    component: SelectSociety,
     // TODO: make this available for public?
     props: true,
     meta: {
