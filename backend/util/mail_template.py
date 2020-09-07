b43d67fea72c236bade0220654154321bae0a06a
Steven Shen <stevenshen1999@hotmail.com>
2020-09-07 22:05:04 +0930

    Added some more changes

--- backend/util/mail_template.py
+++ backend/util/mail_template.py
@@ -115,8 +115,8 @@ def get_login_mail_template(message):
                       <td style="font-family: sans-serif; font-size: 14px; vertical-align: top;">
                         <p style="font-family: sans-serif; font-size: 14px; font-weight: normal; margin: 0; Margin-bottom: 15px;">Hi there,</p>
                         <p style="font-family: sans-serif; font-size: 14px; font-weight: normal; margin: 0; Margin-bottom: 15px;">"""
-    + message + 
-    """</p>
+    + message + """
+                        </p>
                         <table border="0" cellpadding="0" cellspacing="0" class="btn btn-primary" style="border-collapse: separate; mso-table-lspace: 0pt; mso-table-rspace: 0pt; width: 100%; box-sizing: border-box;">
                           <tbody>
                             <tr>
