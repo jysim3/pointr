a408fc585f797d5f0164acc05fc363773c10cf5d
Steven Shen <stevenshen1999@hotmail.com>
2020-09-07 22:14:13 +0930

    [FIX] backend/util/mail_template.py (HTML formatting)

--- backend/util/mail_template.py
+++ backend/util/mail_template.py
@@ -114,9 +114,7 @@ def get_login_mail_template(message):
                     <tr>
                       <td style="font-family: sans-serif; font-size: 14px; vertical-align: top;">
                         <p style="font-family: sans-serif; font-size: 14px; font-weight: normal; margin: 0; Margin-bottom: 15px;">Hi there,</p>
-                        <p style="font-family: sans-serif; font-size: 14px; font-weight: normal; margin: 0; Margin-bottom: 15px;">"""
-    + message + """
-                        </p>
+                        <p style="font-family: sans-serif; font-size: 14px; font-weight: normal; margin: 0; Margin-bottom: 15px;">""" + message + """</p>
                         <table border="0" cellpadding="0" cellspacing="0" class="btn btn-primary" style="border-collapse: separate; mso-table-lspace: 0pt; mso-table-rspace: 0pt; width: 100%; box-sizing: border-box;">
                           <tbody>
                             <tr>
